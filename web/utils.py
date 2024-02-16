081f73ad2e6cd4954496a3512ee4ce81d6b27711
Seyran <106960685+NaShAdIR@users.noreply.github.com>
2024-02-17 00:11:28 +0300

    Update utils.py

--- web/utils.py
+++ web/utils.py
@@ -1,6 +1,23 @@
 from typing import Union
 
 
+def to_sep_corrent(url: str) -> str:
+    return url.replace('//', '/')
+
+
+def merge_url(url_1: str, url_2: str) -> str:
+    return to_sep_corrent(f'{url_1}{url_2}')
+
+
+def to_correct(url: str) -> str:
+    if not url.startswith('/'):
+        url = f'/{url}'
+    if not url.endswith('/'):
+        url = f'{url}/'
+
+    return url
+
+
 def decode(string: Union[bytes, str]) -> str:
     if isinstance(string, bytes):
         return string.decode()
@@ -15,15 +32,17 @@ def encode(string: Union[bytes, str]) -> bytes:
     return string
 
 
-def request_trace(request) -> None:
-    print(
-        f'\033[34m{request.addr} '
-        f'\033[36m-> '
-        f'\033[32m'
-        f'"{request.method} '
-        f'{request.original_path} '
-        f'{request.http_version}" '
-    )
+def request_trace(request, app) -> None:
+    if (app.settings.TRACING
+            and app.server_type != 'uvicorn'):
+        print(
+            f'\033[34m{request.client[0]} '
+            f'\033[36m-> '
+            f'\033[32m'
+            f'"{request.method} '
+            f'{request.path} '
+            f'{request.http_version}" '
+        )
 
 
 def lunch_trace(app):
@@ -32,10 +51,10 @@ def lunch_trace(app):
         if app.settings.SERVER_PORT != '0.0.0.0'
         else 'localhost'}:{app.settings.SERVER_PORT}'
     )
-    print(
-        f'Launched {app.settings.APP_NAME} application:\n'
-        f'  Running on the http://{host}.\n'
-        f'  Installed applications:'
-    )
+    print(f'Launched {app.settings.APP_NAME} application:')
+    print(f'  Running on the http://{host}.')
+    print(f'  Installed applications:')
+    print('    -\n' if len(app.settings.applications.items()) == 0 else '', end='')
+
     for app_name, app in app.settings.applications.items():
-        print(f'    - {app_name}\n')
+        print(f'    - {app_name}')
