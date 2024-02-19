b17d8d5ef31e74dcd9c16161768ea3e996ce115a
Seyran <106960685+NaShAdIR@users.noreply.github.com>
2024-02-19 23:44:43 +0300

    Delete web directory

--- web/http/exceptions.py
+++
@@ -1,8 +0,0 @@
-class EmptyHttp(Exception):
-    def __init__(self):
-        super().__init__('Empty http.')
-
-
-class HttpSyntaxError(Exception):
-    def __init__(self):
-        super().__init__('Http syntax error.')
